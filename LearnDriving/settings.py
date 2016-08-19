969f97657751461585c6d893123ceb0d69053e89
tu95ctv <you@example.com>
2016-08-20 02:28:07 +0700

    ok

--- LearnDriving/settings.py
+++ LearnDriving/settings.py
@@ -18,7 +18,7 @@ ADMINS = (
 MANAGERS = ADMINS
 
 
-#1234
+#12345
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
